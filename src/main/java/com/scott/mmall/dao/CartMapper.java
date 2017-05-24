a78fac51e512a802ead4b30ef9cb98825f78dc67
YangKangJian <2567383143@qq.com>
2017-05-24 09:39:07 +0800

    项目配置文件初始化

---
+++ src/main/java/com/scott/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.scott.mmall.dao;
+
+import com.scott.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
